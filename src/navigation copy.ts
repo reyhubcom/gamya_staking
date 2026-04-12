16e31ca6d51b19f99526e5155a23ed7a79654551
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2026-04-12 17:36:00 -0400

    Create navigation copy.ts

---
+++ src/navigation copy.ts
@@ -0,0 +1,182 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Staking',
+      links: [
+        {
+          text: 'Bitcoin Staking',
+          href: getPermalink('/home/stakeBTC'),
+        },
+        {
+          text: 'Stake [ ETH ]',
+          href: getPermalink('/home/stakeETH'),
+        },
+        {
+          text: 'Stake [ SOL ]',
+          href: getPermalink('/home/stakeSOL'),
+        },
+        {
+          text: 'Stake [ MON ]',
+          href: getPermalink('/home/stakeMON'),
+        },
+      ],
+    },
+    {
+      text: 'Products',
+      links: [
+        {
+          text: 'Bridge Coins',
+          href: getPermalink('/home/bridge'),
+        },
+        {
+          text: 'TEST',
+          href: getPermalink('/services'),
+        },
+        {
+          text: 'Pricing',
+          href: getPermalink('/pricing'),
+        },
+        {
+          text: 'About us',
+          href: getPermalink('/about'),
+        },
+        {
+          text: 'Contact',
+          href: getPermalink('/contact'),
+        },
+        {
+          text: 'Terms',
+          href: getPermalink('/terms'),
+        },
+        {
+          text: 'Privacy policy',
+          href: getPermalink('/privacy'),
+        },
+      ],
+    },
+    {
+      text: 'Protocols',
+      links: [
+        {
+          text: 'DeFi',
+          href: getPermalink('/protocol/defi'),
+        },
+        {
+          text: 'Long-form Sales',
+          href: getPermalink('/landing/sales'),
+        },
+        {
+          text: 'Click-Through',
+          href: getPermalink('/landing/click-through'),
+        },
+        {
+          text: 'Product Details (or Services)',
+          href: getPermalink('/landing/product'),
+        },
+        {
+          text: 'Coming Soon or Pre-Launch',
+          href: getPermalink('/landing/pre-launch'),
+        },
+        {
+          text: 'Subscription',
+          href: getPermalink('/landing/subscription'),
+        },
+      ],
+    },
+    {
+      text: 'Rewards',
+      links: [
+        {
+          text: 'Ethereum',
+         href: getPermalink('/ethereum/reward'),
+        },
+        {
+          text: 'Binance',
+           href: getPermalink('/binance/reward'),
+        },
+        {
+          text: 'Solana',
+          href: getPermalink('/solana/reward'),
+        },
+        {
+          text: 'Polygon',
+           href: getPermalink('/polygon/reward'),
+        },
+        {
+          text: 'All Chains',
+           href: getPermalink('/all/chains'),
+        },
+      ],
+    },
+    {
+      text: 'Wallet',
+      href: '/home/wallet',
+    },
+  ],
+  actions: [{ text: 'EXCHANGE', href: '/home/exchange', target: '' }],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Product',
+      links: [
+        { text: 'Features', href: '#' },
+        { text: 'Security', href: '#' },
+        { text: 'Team', href: '#' },
+        { text: 'Enterprise', href: '#' },
+        { text: 'Customer stories', href: '#' },
+        { text: 'Pricing', href: '#' },
+        { text: 'Resources', href: '#' },
+      ],
+    },
+    {
+      title: 'Platform',
+      links: [
+        { text: 'Developer API', href: '#' },
+        { text: 'Partners', href: '#' },
+        { text: 'Atom', href: '#' },
+        { text: 'Electron', href: '#' },
+        { text: 'AstroWind Desktop', href: '#' },
+      ],
+    },
+    {
+      title: 'Support',
+      links: [
+        { text: 'Docs', href: '#' },
+        { text: 'Community Forum', href: '#' },
+        { text: 'Professional Services', href: '#' },
+        { text: 'Skills', href: '#' },
+        { text: 'Status', href: '#' },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        { text: 'About', href: '#' },
+        { text: 'Blog', href: '#' },
+        { text: 'Careers', href: '#' },
+        { text: 'Press', href: '#' },
+        { text: 'Inclusion', href: '#' },
+        { text: 'Social Impact', href: '#' },
+        { text: 'Shop', href: '#' },
+      ],
+    },
+  ],
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms') },
+    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
+  ],
+  footNote: `
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com"> Rey</a> · 2026 Gamya.xyz All Rights Reserved.
+  `,
+};
